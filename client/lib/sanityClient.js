f61147620081538766926a2c18df53cdb8848c0d
José Martín Rodriguez Mortaloni <76118394+JoseMRodriguezM@users.noreply.github.com>
2022-03-21 21:46:04 -0300

    Update sanityClient.js

--- client/lib/sanityClient.js
+++ client/lib/sanityClient.js
@@ -5,6 +5,6 @@ export const client = sanityClient({
   dataset: 'production',
   apiVersion: 'v1',
   token:
-    'skPI53trwVfw5Ex0dzioGJMclqjo0au6QmY2zcksVe4SajHBp0JartC4PCt0YABdVmBxYFg5mwtbAdGkexh2VgQ766HzoIE1Ie7L4l26MZ7NZ5bHTM1AyM8jAMNBmTtQ4sRZhswbHKDEuS5cTgPnnih6WzhlA2221Y8lZawh2aWg0gWsw11y',
+    'PRIVATE_API_TOKEN',
   useCdn: false,
 })
