1264b8316fed75ac4fd9b0bc7c1b3d14a8779384
José Martín Rodriguez Mortaloni <76118394+JoseMRodriguezM@users.noreply.github.com>
2022-03-21 21:43:07 -0300

    Update hardhat.config.js

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -4,8 +4,8 @@ module.exports = {
     solidity: "0.8.4",
     networks: {
         rinkeby: {
-            url: "https://eth-rinkeby.alchemyapi.io/v2/1lx1nm5naxcZIGAi5-OTRGHosWmU3qL0",
-            accounts: ["e347bad3022f98dd03b4c91191540cb0391a0757bea2aea93e8ede1bb2a70f1c"]
+            url: "PRIVATE_LINK",
+            accounts: ["PRIVATE_KEY"]
         },
     },
 
